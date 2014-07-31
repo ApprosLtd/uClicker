4b11ecb9908b05608243f264621b426bd8e33b8d
Vitaly Serov <serovvitaly@gmail.com>
2014-07-31 16:46:02 +0400

    DashboardController

---
+++ app/controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitaly
+ * Date: 31.07.14
+ * Time: 16:40
+ */
+
+class DashboardController extends BaseController {
+
+    public function getIndex()
+    {
+        return 'DASHBOARD';
+    }
+
+} 
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', array('before' => 'auth', 'uses' => 'DashboardController@getIndex'));
+
+Route::get('/', 'HomeController@getIndex');
