2fa2308ed2483efb1411c6a7ff379be7fa6073e0
Vitaly Serov <serovvitaly@gmail.com>
2014-08-08 12:12:21 +0400

    commit

--- app/views/frame.php
+++ app/views/frame.php
@@ -5,13 +5,19 @@
     <title>Untitled</title>
 </head>
 <body>
-<script src="//vk.com/js/api/openapi.js" type="text/javascript"></script>
+<!--script src="//vk.com/js/api/openapi.js" type="text/javascript"></script-->
 <script type="text/javascript">
+
+
+    var url = 'http://vk.com/al_apps.php?act=wall_post_box&widget=1&method=wall.post&aid=4335971&text=<?= $text ?>&attachments=<?= $href ?>';
+
+    window.location = url;
+    /*
     VK.init({
         apiId: 4335971
     });
 
-    //VK.Auth.logout();
+    VK.Auth.logout();
 
     VK.Auth.login(function(data){
         console.log(data);
@@ -23,14 +29,16 @@
 
         window.location = url;
 
-        /*
+
         VK.Api.call('wall.post', {
             message: '<?= $text ?>',
             attachments: '<?= $href ?>'
         }, function(data) {
             console.log(data);
-        });*/
+        });
     });
+
+    */
 
 </script>
 
