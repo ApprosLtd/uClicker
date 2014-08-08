1dafb888ddd4cc128bdad47fd0e26f28b792c297
Vitaly Serov <serovvitaly@gmail.com>
2014-08-08 12:33:09 +0400

    commit

--- public/connect.js
+++ public/connect.js
@@ -6,7 +6,7 @@ function UCL(){
 UCL.prototype.linck = function(text, href){
     var url = 'http://weber.appros.ru/frame?text='+text+'&href='+href;
     this.winObj = window.open(url, this.winName, "width=420,height=230,resizable=yes,scrollbars=yes,status=no,location=no,menubar=no");
-    this.winObj.onbeforeunload(function(){
+    this.winObj.addEventListener('onunload', function(){
         alert('OnBeforeUnload');
     });
 }
