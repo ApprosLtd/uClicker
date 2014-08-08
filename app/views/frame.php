c0182c311f6d3d79e381aafa52fc7dcf5344aae2
Vitaly Serov <serovvitaly@gmail.com>
2014-08-08 12:09:20 +0400

    commit

--- app/views/frame.php
+++ app/views/frame.php
@@ -18,12 +18,18 @@
     }, 8192);
 
     VK.Auth.getLoginStatus(function(status){
+
+        var url = 'http://vk.com/al_apps.php?act=wall_post_box&widget=1&method=wall.post&aid=4335971&text=<?= $text ?>&attachments=<?= $href ?>';
+
+        window.location = url;
+
+        /*
         VK.Api.call('wall.post', {
             message: '<?= $text ?>',
             attachments: '<?= $href ?>'
         }, function(data) {
             console.log(data);
-        });
+        });*/
     });
 
 </script>
