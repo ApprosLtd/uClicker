d80321361be2b5983a93eaa236bb5621bca99388
Vitaly Serov <serovvitaly@gmail.com>
2014-08-08 23:16:22 +0400

    commit

--- app/helpers/Secure.php
+++ app/helpers/Secure.php
@@ -5,11 +5,8 @@ class Secure
     
     public static function makeToken()
     {
-        $token = uniqid('', true);
-        
-        $token = str_replace('.', '', $token);
-        
-        return $token;
+        $token = uniqid('', true);        
+        return sha1($token);
     }
     
 }
