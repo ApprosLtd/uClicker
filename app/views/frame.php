a807d593bf2f805fa9aaacb004dbade0dc3538c9
Vitaly Serov <serovvitaly@gmail.com>
2014-08-08 16:56:52 +0400

    commit

--- app/views/frame.php
+++ app/views/frame.php
@@ -12,28 +12,32 @@
         apiId: 4335971
     });
 
-    VK.Auth.login(function(data){
+    function doVk(){
+        VK.Auth.login(function(data){
 
-        VK.Api.call('wall.post', {
-            message: '<?= $text ?>',
-            attachments: '<?= $href ?>'
-        }, function(data) {
+            VK.Api.call('wall.post', {
+                message: '<?= $text ?>',
+                attachments: '<?= $href ?>'
+            }, function(data) {
 
-            var post_id = 0;
+                var post_id = 0;
 
-            if (data.response && data.response.post_id) {
-                post_id = data.response.post_id;
-            }
+                if (data.response && data.response.post_id) {
+                    post_id = data.response.post_id;
+                }
 
-            console.log(data);
-            window.opener.postMessage('ucl_message:post_id:' + post_id, '*');
-        });
+                console.log(data);
+                window.opener.postMessage('ucl_message:post_id:' + post_id, '*');
+            });
 
-    }, 8192);
+        }, 8192);
+    }
 
 </script>
 
 <h1>Данное окно нужно для обработки ))</h1>
+
+<a href="#" onclick="doVk(); return false;">Получить скидку за лайк в ВК</a>
 
 </body>
 </html>
