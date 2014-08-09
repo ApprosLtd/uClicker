a412eb804f3c7d902ba64efb09b732aa76e93f56
Vitaly Serov <serovvitaly@gmail.com>
2014-08-09 06:59:07 +0400

    commit

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends BaseController {
         return Redirect::to('/');
     }
 
-    public function getMerchants()
+    public function getVisitors()
     {
         return Redirect::to('/');
     }
