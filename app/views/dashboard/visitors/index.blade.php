6ffbfd9b8936161fa8bfebbae1cce8480d7f2bb5
Vitaly Serov <serovvitaly@gmail.com>
2014-08-10 21:01:24 +0400

    commit

--- app/views/dashboard/visitors/index.blade.php
+++ app/views/dashboard/visitors/index.blade.php
@@ -4,7 +4,7 @@
         <th>Посетитель</th>
         <th style="width: 200px">Постов</th>
         <th style="width: 200px">Соц.сеть</th>
-        <th style="width: 270px"></th>
+        <!--th style="width: 270px"></th-->
     </tr>
     </thead>
     <tbody>
@@ -12,17 +12,17 @@
     foreach ($visitors as $visitor) {
         ?>
         <tr>
-            <td><a href="http://vk.com/id<?= $visitor->uid ?>" target="_blank"><?= $visitor->first_name . ' ' . $visitor->last_name ?></a></td>
+            <td><a href="#" data-toggle="modal" data-target="#visitorInformation" onclick="visitorInfo(<?= $visitor->id ?>); return false;"><?= $visitor->first_name . ' ' . $visitor->last_name ?></a></td>
             <td><?= $visitor->quests ?></td>
             <td><?= $visitor->vendor ?></td>
-            <td>
+            <!--td>  
                 <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#visitorInformation" onclick="visitorInfo(<?= $visitor->id ?>)">подробнее</button>
-                <!-- <? if ($visitor->blocked) { ?>
+                <? if ($visitor->blocked) { ?>
                     <button type="button" class="btn btn-default btn-xs" onclick="visitorUnblocking(<?= $visitor->id ?>)" title="разблокировать">разблок-вать</button>
                 <? } else { ?>
                     <button type="button" class="btn btn-warning btn-xs" onclick="visitorBlocking(<?= $visitor->id ?>)">блокировать</button>
-                <? } ?>   -->
-            </td>
+                <? } ?> 
+            </td-->
         </tr>
     <?
     }
@@ -39,7 +39,7 @@
             </div>
             <div class="modal-body">
 
-                Посетитель
+                <a href="http://vk.com/id<?= $visitor->uid ?>" target="_blank">Посетитель</a>
 
             </div>
         </div>
