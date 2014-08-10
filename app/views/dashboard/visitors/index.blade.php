7ceabc6ef14f293c0d46196fd29421c53ee3a8b4
Vitaly Serov <serovvitaly@gmail.com>
2014-08-10 20:36:14 +0400

    commit

--- app/views/dashboard/visitors/index.blade.php
+++ app/views/dashboard/visitors/index.blade.php
@@ -14,7 +14,7 @@
             <td><a href="http://vk.com/id<?= $visitor->uid ?>" target="_blank"><?= $visitor->first_name . ' ' . $visitor->last_name ?></a></td>
             <td><?= $visitor->vendor ?></td>
             <td>
-                <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#visitorInformation" onclick="visitorInfo(<?= $site->id ?>)">подробнее</button>
+                <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#visitorInformation" onclick="visitorInfo(<?= $visitor->id ?>)">подробнее</button>
                 <? if ($visitor->blocked) { ?>
                     <button type="button" class="btn btn-default btn-xs" onclick="visitorUnblocking(<?= $visitor->id ?>)" title="разблокировать">разблок-вать</button>
                 <? } else { ?>
