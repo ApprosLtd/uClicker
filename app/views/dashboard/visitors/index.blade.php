deffa6bde0b8d6c15ec1cecba1345de86c55ee5d
Vitaly Serov <serovvitaly@gmail.com>
2014-08-10 20:56:43 +0400

    commit

--- app/views/dashboard/visitors/index.blade.php
+++ app/views/dashboard/visitors/index.blade.php
@@ -17,11 +17,11 @@
             <td><?= $visitor->vendor ?></td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#visitorInformation" onclick="visitorInfo(<?= $visitor->id ?>)">подробнее</button>
-                <? if ($visitor->blocked) { ?>
+                <!-- <? if ($visitor->blocked) { ?>
                     <button type="button" class="btn btn-default btn-xs" onclick="visitorUnblocking(<?= $visitor->id ?>)" title="разблокировать">разблок-вать</button>
                 <? } else { ?>
                     <button type="button" class="btn btn-warning btn-xs" onclick="visitorBlocking(<?= $visitor->id ?>)">блокировать</button>
-                <? } ?>
+                <? } ?>   -->
             </td>
         </tr>
     <?
@@ -29,3 +29,19 @@
     ?>
     </tbody>
 </table>
+
+<div class="modal fade" id="visitorInformation" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Закрыть</span></button>
+                <h4 class="modal-title">Информация о посетителе</h4>
+            </div>
+            <div class="modal-body">
+
+                Посетитель
+
+            </div>
+        </div>
+    </div>
+</div>
