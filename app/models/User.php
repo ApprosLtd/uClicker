e7cec1f5583a96748dca2c2dca09ad3200b3f2b3
Vitaly Serov <serovvitaly@gmail.com>
2014-08-15 23:32:20 +0400

    Права и пользователи

--- app/models/User.php
+++ app/models/User.php
@@ -23,6 +23,66 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+    public function roles()
+    {
+        return $this->belongsToMany('Role', 'users_roles');
+    }
+    
+    public function hasRole($check)
+    {
+        return in_array($check, array_fetch($this->roles->toArray(), 'name'));
+    }
+    
+    /**
+     * Find out if User is an employee, based on if has any roles
+     *
+     * @return boolean
+     */
+    public function isEmployee()
+    {
+        $roles = $this->roles->toArray();
+        return !empty($roles);
+    }
+    
+    /**
+     * Get key in array with corresponding value
+     *
+     * @return int
+     */
+    private function getIdInArray($array, $term)
+    {
+        foreach ($array as $key => $value) {
+            if ($value == $term) {
+                return $key;
+            }
+        }
+ 
+        throw new UnexpectedValueException;
+    }
+    
+    /**
+     * Add roles to user to make them a concierge
+     */
+    public function makeEmployee($name)
+    {
+        $assigned_roles = array();
+ 
+        $roles = array_fetch(Role::all()->toArray(), 'name');
+ 
+        switch ($name) {
+            case 'super_admin':
+                $assigned_roles[] = $this->getIdInArray($roles, 'edit_customer');
+                $assigned_roles[] = $this->getIdInArray($roles, 'delete_customer');
+            case 'admin':
+                $assigned_roles[] = $this->getIdInArray($roles, 'create_customer');
+            default:
+                throw new \Exception("The employee status entered does not exist");
+        }
+ 
+        $this->roles()->attach($assigned_roles);
+    }
+    
+    
 
     public function sites()
     {
