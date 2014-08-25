36aeb3834c1c18500101d672334546bc58f45ddf
Vitaly Serov <serovvitaly@gmail.com>
2014-08-25 19:22:32 +0400

    frame

--- app/views/frame/index.php
+++ app/views/frame/index.php
@@ -4,38 +4,41 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Скидочный сервис uClicker</title>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="/packages/bootstrap/css/bootstrap.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic,latin-ext" rel="stylesheet" type="text/css">
+    <style>
+        .logo-title{
+            font-family: 'Lobster', cursive;
+            text-align: center;
+        }
+        .row-ico{
+            width: 374px;
+            margin: 10px auto;
+        }
+        .col-ico{
+            display: inline-block;
+            margin: 5px;
+        }
+    </style>
 </head>
 <body>
 <script src="//vk.com/js/api/openapi.js" type="text/javascript"></script>
 <script type="text/javascript">
-
     VK.init({
         apiId: 4335971
     });
-
     function doVk(){
         VK.Auth.login(function(data){
-            
             console.log(data);
-            
             var visitor_uid = 0;
-            
-            if (data.session && data.session.user && data.session.user.id) {
-                visitor_uid = data.session.user.id;
-            }
-
+            if (data.session && data.session.user && data.session.user.id) visitor_uid = data.session.user.id;
             VK.Api.call('wall.post', {
                 message: '<?= $text ?>',
                 attachments: '<?= $href ?>'
             }, function(data) {
-
                 var post_id = 0;
-
                 if (data.response && data.response.post_id) {
                     post_id = data.response.post_id;
                 }
-
                 if (post_id > 0) {
                     $.ajax({
                         url: '/connect/success',
@@ -53,31 +56,22 @@
                 } else {
                     // TODO: error message
                 }
-
                 //window.opener.postMessage('ucl_message:post_id:' + post_id, '*');
             });
-
         }, 8192);
     }
-
 </script>
-
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
-            <h1>uClicker</h1>
+            <h1 class="logo-title">uClicker</h1>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-2">VK</div>
-        <div class="col-lg-2">FB</div>
-        <div class="col-lg-2">OK</div>
-        <div class="col-lg-3"></div>
+    <div class="row-ico" style="width: ">
+        <div class="col-ico"><a href="#"><img src="/packages/socico/vk-128.png" alt="" onclick="doVk(); return false;"></a></div>
+        <div class="col-ico"><a href="#"><img src="/packages/socico/ok-128.png" alt="" onclick="doVk(); return false;"></a></div>
+        <div class="col-ico"><a href="#"><img src="/packages/socico/fb-128.png" alt="" onclick="doVk(); return false;"></a></div>
     </div>
 </div>
-
-<a href="#" onclick="doVk(); return false;">Получить скидку за лайк в ВК</a>
-
 </body>
 </html>
