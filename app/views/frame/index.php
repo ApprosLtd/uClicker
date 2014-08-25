1c35a7f9e328a332718610cd78512f10551059bf
Vitaly Serov <serovvitaly@gmail.com>
2014-08-25 20:50:59 +0400

    facebook app

--- app/views/frame/index.php
+++ app/views/frame/index.php
@@ -50,6 +50,13 @@
             version    : 'v2.0'
         });
     };
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
     function doVk(){
         VK.Auth.login(function(data){
