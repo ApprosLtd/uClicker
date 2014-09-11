ddb38218c7abd6a29cd9e86654b54a39701de1b8
Vitaly Serov <serovvitaly@gmail.com>
2014-09-11 10:41:17 +0400

    frame vk

--- app/controllers/ConnectController.php
+++ app/controllers/ConnectController.php
@@ -114,7 +114,7 @@ class ConnectController extends BaseController {
         $image_url  = Input::get('image_url');
         $upload_url = Input::get('upload_url');
 
-        $img_binary = fread(fopen($image_url, "r"), filesize($image_url));
+        $img_binary = file_get_contents($image_url);
 
         $response = file_get_contents($upload_url, false, stream_context_create(array(
             'http' => array(
